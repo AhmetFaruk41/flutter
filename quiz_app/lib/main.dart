9600764c5984f6df77ac80d10f4f1ff121d84b08
Ahmet Faruk ÇATLAR <ahmetfaruk411@gmail.com>
2024-04-02 03:28:32 +0300

    flutter

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'GradientContainer.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: GradientContainer(
-          Color.fromARGB(206, 7, 26, 87), Color.fromARGB(255, 121, 233, 99)),
-    ),
-  ));
+  runApp(const Quiz());
 }
