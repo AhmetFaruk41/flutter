05a45b559a632f5a543a09ec48d3f2b2d53c70b9
Ahmet Faruk Çatlar <119080744+AhmetFaruk41@users.noreply.github.com>
2024-05-12 01:15:54 +0300

    Update quiz.dart

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -64,8 +64,8 @@ class _QuizState extends State<Quiz> {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
+                Color.fromARGB(255, 80, 13, 151),
+                Color.fromARGB(255, 110, 15, 168),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
